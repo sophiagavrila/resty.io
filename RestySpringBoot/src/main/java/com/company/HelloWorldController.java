44dee3701ced2368c089ed91133aff07d535bf06
sophiagavrila <msophiagavrila@gmail.com>
2021-01-12 12:50:19 -0500

    add templates + controller

---
+++ RestySpringBoot/src/main/java/com/company/HelloWorldController.java
@@ -0,0 +1,25 @@
+package com.company;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class HelloWorldController {
+	
+	@RequestMapping("/hello")
+	public ModelAndView showMessage(@RequestParam(value = "name", required = false, defaultValue = "World") String name) {
+		System.out.println("From controller...");
+		
+		ModelAndView mv = new ModelAndView("hello");
+		mv.addObject("hello", "Hello");
+		mv.addObject("name", name);
+		
+		return mv;
+		
+		
+		
+	}
+
+}
